8e6e8f6851f808abd3c8cfdfdcfa0c25493f42cf
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-10-22 16:05:59 -0400

    oled and gpio and ble alive

--- code.py
+++ code.py
@@ -3,33 +3,63 @@ import busio
 from digitalio import DigitalInOut, Direction, Pull
 
 from adafruit_mcp230xx.mcp23017 import MCP23017
-
 from kmk.hid import HIDModes
 from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.modules.layers import Layers
 from kmk.scanners import DiodeOrientation
 from kmk.scanners.digitalio import MatrixScanner
+from kmk.modules.mouse_keys import MouseKeys
+# from kmk.modules.pmw3360 import PMW3360
+import gc
+
+gc.collect()
+print(str(gc.mem_free())+"        ")
+
+i2c = busio.I2C(scl=board.D1, sda=board.D0, frequency=100000)
+
+from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
+oled_ext = Oled(
+    OledData(
+        corner_one={0:OledReactionType.STATIC,1:["layer"]},
+        corner_two={0:OledReactionType.LAYER,1:["1","2","3","4"]},
+        corner_three={0:OledReactionType.LAYER,1:["base","raise","lower","adjust"]},
+        corner_four={0:OledReactionType.LAYER,1:["qwerty","nums","shifted","leds"]}
+        ),
+        toDisplay=OledDisplayMode.TXT,flip=False)
+
 
 # DEBUG_ENABLE = True
+print("starting")
 
 
-i2c = busio.I2C(scl=board.SCL, sda=board.SDA, frequency=100000)
 mcp = MCP23017(i2c, address=0x20)
 class MyKeyboard(KMKKeyboard):
     def __init__(self):
         # create and register the scanner
         self.matrix = MatrixScanner(
-            cols=(mcp.get_pin(12), mcp.get_pin(13), mcp.get_pin(14), mcp.get_pin(15), mcp.get_pin(4), mcp.get_pin(5), mcp.get_pin(6), mcp.get_pin(7), mcp.get_pin(3), mcp.get_pin(2), mcp.get_pin(1)),
-            rows=(board.D7, board.D8, board.D9, board.D10),
+            cols=(mcp.get_pin(0), mcp.get_pin(1), mcp.get_pin(2), mcp.get_pin(3), mcp.get_pin(4), mcp.get_pin(5), mcp.get_pin(6), mcp.get_pin(7), mcp.get_pin(8), mcp.get_pin(9), mcp.get_pin(10)),
+            rows=(mcp.get_pin(11), mcp.get_pin(12), mcp.get_pin(13), mcp.get_pin(14)),
             diode_orientation=DiodeOrientation.COLUMNS,
-            pull=Pull.DOWN,
+            pull=Pull.UP,
             rollover_cols_every_rows=None, # optional
         )
 
 keyboard = MyKeyboard()
-layer_ext = Layers
-keyboard.modules = [layer_ext]
+keyboard.SCL = board.SCL
+keyboard.SDA = board.SDA
+layer_ext = Layers()
+
+# gc.collect()
+# print(str(gc.mem_free())+" gc before pmw3360       ")
+
+# pmw3360 = PMW3360(cs=board.D7, miso=board.MISO, mosi=board.MOSI, sclk=board.SCK, invert_x=True, invert_y=True, flip_xy=True)
+
+# gc.collect()
+# print(str(gc.mem_free())+" gc AFTER pmw3360       ")
+
+# if str(getmount('/').label).endswith('R'):
+keyboard.modules = [layer_ext, oled_ext, MouseKeys()]
 
 _______ = KC.TRNS
 XXXXXXX = KC.NO
@@ -37,9 +67,8 @@ XXXXXXX = KC.NO
 FN = KC.MO(1)
 
 keyboard.debug_enabled = True
-
-keyboard.col_pins = (mcp.get_pin(12), mcp.get_pin(13), mcp.get_pin(14), mcp.get_pin(15), mcp.get_pin(4), mcp.get_pin(5), mcp.get_pin(6), mcp.get_pin(7), mcp.get_pin(3), mcp.get_pin(2), mcp.get_pin(1))
-keyboard.row_pins = (board.D7, board.D8, board.D9, board.D10)
+#]keyboard.col_pins = (board.D0, board.D1)
+# keyboard.row_pins = (board.D2, board.D3)
 keyboard.diode_orientation = DiodeOrientation.COLUMNS
 
 keyboard.keymap = [
@@ -79,9 +108,11 @@ keyboard.keymap = [
         KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F,      KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN,
         KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,      KC.B,    KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH
     ]
-
 ]
 
 if __name__ == '__main__':
-    keyboard.go(hid_type=HIDModes.BLE, ble_name='Wyld')
-    # keyboard.go(hid_type=HIDModes.USB)
+    print("In Main")
+    gc.collect()
+    print(str(gc.mem_free())+" after init       ")
+    keyboard.go(hid_type=HIDModes.BLE, ble_name='KMKeyboard')
+
